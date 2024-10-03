7341a16542262ac4a762b6db670daee6b2b67a3d
Mariel M <mariemunoz2022@udec.cl>
2024-10-02 22:49:47 -0300

    Se agregó el retorno de sabor del producto (utilizando getNombre desde Enum

--- src/Comprador.java
+++ src/Comprador.java
@@ -3,30 +3,29 @@ import Monedas.*;
 import Productos.*;
 
 class Comprador {
- //   private String sonido;
+    private String saborProducto;
     private int vuelto;
 
     public Comprador(Moneda m, productosEnum cualProducto, Expendedor exp) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException{
         Productos producto = exp.comprarProducto(m, cualProducto);
         vuelto = 0;
-        if (producto != null) { //si el producto no es null
-            m = exp.getVuelto(); //la moneda recibida m del deposito vuelto
-            while (m != null) { //mientras hayan monedas en el deposito
+        if (producto != null) {
+            saborProducto = cualProducto.getNombre();
+            m = exp.getVuelto();
+            while (m != null) {
                 vuelto = vuelto + m.getValor(); //el vuelto es igual al vuelto mas el valor de la moneda
                 m = exp.getVuelto(); //m es igual al vuelto
             }
-          //  sonido = bebida.beber();
         } else {
             if (m == null) { //si no hay monedas en el deposito de vuelto
                 vuelto = 0;//vuelto es 0
-              //  sonido = null;
+                saborProducto = null;
             } else {
                 m = exp.getVuelto(); //en el caso de que el producto sea null, m es igual al vuelto
                 while (m != null) { //mientras hayan monedas en el deposito
                     vuelto = vuelto + m.getValor();
                     m = exp.getVuelto();
                 }
-             //   sonido = null;
             }
         }
     }
@@ -34,8 +33,8 @@ class Comprador {
     public int cuantoVuelto() {
         return vuelto;
     }
-    //Agregar sonidos
-   // public String queBebiste() {
-       // return sonido;
-    //}
+
+    public String getSaborProducto() {
+        return saborProducto;
+    }
 }
