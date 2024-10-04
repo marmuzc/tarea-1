d456e523b1485c0cdc59b3b702f9e1f04634cb10
Mariel M <mariemunoz2022@udec.cl>
2024-10-03 23:00:10 -0300

    Implemetacion de comentarios formato javadoc

--- src/Productos/Productos.java
+++ src/Productos/Productos.java
@@ -32,7 +32,7 @@ public abstract class Productos {
     public abstract String getNombre();
 
     @Override
-    public String toString() {
+    public String toString() { // Se sobreescribe el método toString para que muestre el nombre y la serie del producto.
         return "Nombre: " + this.getNombre() + " Serie: " + this.getSerie() + ".";
     }
 }
